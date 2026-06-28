fac001b2bb2da36274bb44aead245e92aadae07e
aiiddqd <1852897+aiiddqd@users.noreply.github.com>
2026-06-28 15:54:15 +0300

    feat: add GitHub Actions workflow for deploying Astro site to GitHub Pages

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const repository = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const repositoryOwner = process.env.GITHUB_REPOSITORY_OWNER;
+const isGitHubPagesBuild = process.env.GITHUB_PAGES === 'true' && repository && repositoryOwner;
+
+const base = isGitHubPagesBuild ? `/${repository}/` : '/';
+const site = isGitHubPagesBuild ? `https://${repositoryOwner}.github.io/${repository}/` : undefined;
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	base,
+	site,
+});
